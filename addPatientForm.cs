311728ba3fe1e0bd47a53ca4e8e0ea46b3d3ec1f
Mohamed Thabet <thabetx@gmail.com>
2015-10-24 14:04:41 +0200

    added the add patient form

---
+++ addPatientForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_Assistant
+{
+    public partial class AddPatientForm : Form
+    {
+        public AddPatientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var patient = new Domain.Patient
+            {
+                name = textBox1.Text,
+                phone = textBox2.Text,
+                gender = textBox3.Text,
+            };
+            PatientService patientService = new PatientService();
+            patientService.save(patient);
+        }
+    }
+}
