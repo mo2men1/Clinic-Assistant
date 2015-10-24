36ea5e43aac9921729c3baaff6cdbd3cf016d916
Mohamed Thabet <thabetx@gmail.com>
2015-10-24 13:47:16 +0200

    adds session provider

--- Service/PatientService.cs
+++ Service/PatientService.cs
@@ -6,36 +6,23 @@ using System.Threading.Tasks;
 using NHibernate;
 using NHibernate.Cfg;
 using Clinic_Assistant.Domain;
+
 namespace Clinic_Assistant
 {
    class PatientService
    {
       public void save(Patient p)
       {
-         var cfg = new Configuration();
-         cfg.Configure();
-         cfg.AddAssembly(typeof(Domain.Patient).Assembly);
-
-         // Get ourselves an NHibernate Session
-         var sessions = cfg.BuildSessionFactory();
-         var sess = sessions.OpenSession();
-
+         var sess = SessionProvider.createSession();
          sess.Save(p);
          sess.Flush();
       }
 
       public IList <Patient> getPatient()
       {
-         var cfg = new Configuration();
-         cfg.Configure();
-         cfg.AddAssembly(typeof(Domain.Patient).Assembly);
-
-         var sessions = cfg.BuildSessionFactory();
-         var sess = sessions.OpenSession();
-
+         var sess = SessionProvider.createSession();
          IQuery q = sess.CreateQuery("FROM Patient");
          return  q.List<Domain.Patient>();
-        // list.ToList().ForEach(p => Console.WriteLine(p.name));
       }
    }
 }
--- SessionProvider.cs
+++ SessionProvider.cs
@@ -6,22 +6,21 @@ using System.Threading.Tasks;
 using NHibernate;
 using NHibernate.Cfg;
 
-
-
 namespace Clinic_Assistant
 {
-   public class SessionProvider
-   {
-      public ISession createSession() {
-      var cfg = new Configuration();
-         cfg.Configure();
-         cfg.AddAssembly(typeof(Domain.Patient).Assembly);
+    public class SessionProvider
+    {
+        static public ISession createSession()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Domain.Patient).Assembly);
 
-         // Get ourselves an NHibernate Session
-         var sessions = cfg.BuildSessionFactory();
-         var sess = sessions.OpenSession();
+            // Get ourselves an NHibernate Session
+            var sessions = cfg.BuildSessionFactory();
+            var sess = sessions.OpenSession();
 
-         return sess;
-      }
-   }
+            return sess;
+        }
+    }
 }
