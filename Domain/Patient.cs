41beb3d38eb60c244e78d797643f02ac772d485d
Mohamed Thabet <thabetx@gmail.com>
2015-11-02 20:51:18 +0200

    adds visit class and relation with patient

--- Domain/Patient.cs
+++ Domain/Patient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 //using System.Collections.Generic;
 //using System.Linq;
 //using System.Text;
@@ -15,5 +16,6 @@ namespace Clinic_Assistant.Domain
         public virtual DateTime dateOfBirth { get; set; }
         public virtual Nullable<int> age { get; set; }
         public virtual string medical_history { get; set; }
+        public virtual IList<Visit> visits { get; set; } 
     }
 }
---
+++ Domain/Visit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Clinic_Assistant.Domain
+{
+    public class Visit
+    {
+        public virtual int id { get; set; }
+        public virtual string purpose { get; set; }
+        public virtual DateTime date { get; set; }
+        public virtual Patient patient { get; set; }
+    }
+}
+
+
